c385126c7e34a46fb92946b380cb0840abd89543
El profe <al.sepulvedam@profesor.duoc.cl>
2026-03-23 11:15:15 -0300

    clase libro agregada

--- src/main/java/com/example/bibliotecaduoc/model/Libro.java
+++ src/main/java/com/example/bibliotecaduoc/model/Libro.java
@@ -1,5 +1,18 @@
 package com.example.bibliotecaduoc.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Libro {
     // Atributos de la clase Libro
+    private int id;
+    private String isbn;
+    private String titulo;
+    private String editorial;
+    private int fechaPublicacion;
+    private String autor;   
 }
